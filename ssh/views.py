3886d851f901975f3d2e21dde27171a187dfbd45
Harry Potter <random@gmail.com>
2022-07-17 23:23:47 -0700

    all

--- ssh/views.py
+++ ssh/views.py
@@ -16,7 +16,7 @@ def create(request):
 	bid.name = data['name']
 	bid.phone = data['phone']
 	bid.language = data['language']
-	bid.source = 1
+	bid.source = data['source']
 	bid.save()
 
 	return JsonResponse(200, safe = False)
